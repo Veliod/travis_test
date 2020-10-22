e9a471ee507bfb224fcc9d809aad57353b213edc
Veliod <57393252+Veliod@users.noreply.github.com>
2020-10-22 15:23:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-def func(x):
-    return x + 1
 
-def test_answer():
-    assert func(3) == 4
+def test_greater():
+   num = 100
+   assert num > 101
 
-if __name__ == "__main__":
-    test_answer()
+test_greater()
